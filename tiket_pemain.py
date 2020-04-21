984cdc577c071e0684ed8943af456edcd2ac769d
isabellahandayani <63598464+isabellahandayani@users.noreply.github.com>
2020-04-22 00:20:59 +0700

    Update tiket_pemain.py

--- tiket_pemain.py
+++ tiket_pemain.py
@@ -1,10 +1,11 @@
+
 def tiket_pemain():
     username = input("Masukkan username: ")
     print("Riwayat:")
-    for row in (array_kepemilikan_tiket):
-        if row!= "*": 
-            if username == row["Username"]:
-                for line in (array_wahana):
-                    if line !="*":
-                        if row["ID_Wahana"] == line["ID_Wahana"]:
-                            print(line["ID_Wahana"],"|",line["Nama_Wahana"],"|",row["Jumlah_Tiket"])
+    i = 0
+    while array_kepemilikan_tiket[i] != "*":
+        if username == array_kepemilikan_tiket[i]["Username"]:
+            for line in (array_wahana):
+                if line != "*" and array_kepemilikan_tiket[i]["ID_Wahana"] == line["ID_Wahana"]:
+                        print(line["ID_Wahana"], "|", line["Nama_Wahana"], "|", array_kepemilikan_tiket[i]["Jumlah_Tiket"])
+        i+=1
