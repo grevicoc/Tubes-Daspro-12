e9c478abe9e06375d9fbd1025b9a47b0d747ed3a
isabellahandayani <63598464+isabellahandayani@users.noreply.github.com>
2020-04-21 18:35:47 +0700

    Create riwayat_wahana.py

---
+++ riwayat_wahana.py
@@ -0,0 +1,7 @@
+def riwayat_wahana():
+    wahana = input("Masukkan ID Wahana: ")
+    print("Riwayat:")
+    for row in (array_penggunaan_tiket):
+        if row != "*":
+            if row['ID_Wahana'] == wahana:
+                print(row['Tanggal_Penggunaan'],"|",row['Username'],"|",row['Jumlah_Tiket'])
