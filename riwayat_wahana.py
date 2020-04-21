f25e275706ac6927816abd0b3e68e8b7428681a0
isabellahandayani <63598464+isabellahandayani@users.noreply.github.com>
2020-04-21 22:27:39 +0700

    Update riwayat_wahana.py

--- riwayat_wahana.py
+++ riwayat_wahana.py
@@ -1,7 +1,8 @@
 def riwayat_wahana():
     wahana = input("Masukkan ID Wahana: ")
     print("Riwayat:")
-    for row in (array_penggunaan_tiket):
-        if row != "*": #Pada array_penggunaan_tiket mark == "*"
-            if row['ID_Wahana'] == wahana:
-                print(row['Tanggal_Penggunaan'],"|",row['Username'],"|",row['Jumlah_Tiket'])
+    i = 0
+    while array_penggunaan_tiket[i]!= "*":
+            if array_penggunaan_tiket[i]['ID_Wahana'] == wahana:
+                print(array_penggunaan_tiket[i]['Tanggal_Penggunaan'],"|",array_penggunaan_tiket[i]['Username'],"|",array_penggunaan_tiket[i]['Jumlah_Tiket'])
+            i += 1
