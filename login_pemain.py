3fcd9f77ba53643d1878ef21b3909408f4fd2449
balam15 <64026206+balam15@users.noreply.github.com>
2020-04-21 22:26:29 +0700

    Add files via upload

---
+++ login_pemain.py
@@ -0,0 +1,10 @@
+def Login_User():
+    username = input("Masukan username:")
+    password = input("Masukan password:")
+    for row in (array_user):
+        if row != "*":
+            if((row["Username"] == username) and (row["Password"] == password)):
+                print("Selamat bersenang-senang,",row["Nama"],"!")
+            else:
+                print("Ups, password salah atau kamu tidak terdaftar dalam sistem kami. Silakan coba lagi!")
+                Login_User()
