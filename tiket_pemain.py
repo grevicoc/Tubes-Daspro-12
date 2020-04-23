ac346ca43239f38c62e290d103c8e51245a65204
isabellahandayani <63598464+isabellahandayani@users.noreply.github.com>
2020-04-23 17:54:52 +0700

    Update tiket_pemain.py

--- tiket_pemain.py
+++ tiket_pemain.py
@@ -1,11 +1,14 @@
-
 def tiket_pemain():
     username = input("Masukkan username: ")
     print("Riwayat:")
     i = 0
-    while array_kepemilikan_tiket[i] != "*":
-        if username == array_kepemilikan_tiket[i]["Username"]:
-            for line in (array_wahana):
-                if line != "*" and array_kepemilikan_tiket[i]["ID_Wahana"] == line["ID_Wahana"]:
-                        print(line["ID_Wahana"], "|", line["Nama_Wahana"], "|", array_kepemilikan_tiket[i]["Jumlah_Tiket"])
+    while ((array_kepemilikan_tiket[i] != "*") and (i < 30)):
+        if (username == array_kepemilikan_tiket[i][0]): # Menemukan data username yang dimasukkan
+            j = 0
+            found = False
+            while ((array_wahana[j]!="*")  and not found):
+                if (array_kepemilikan_tiket[i][1] == array_wahana[j][0]): # Menemukan data wahana yang digunakan oleh username terkait
+                        print(array_wahana[j][0], "|", array_wahana[j][1], "|", array_kepemilikan_tiket[i][2])
+                        found = True
+                j +=1
         i+=1
