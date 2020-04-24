ad0683d3fea525738537e3a6ae2491a828728db3
isabellahandayani <63598464+isabellahandayani@users.noreply.github.com>
2020-04-24 16:03:45 +0700

    Update riwayat_wahana.py

--- riwayat_wahana.py
+++ riwayat_wahana.py
@@ -1,8 +1,12 @@
 def riwayat_wahana():
-    id = input("Masukkan ID Wahana: ")#Asumsi masukan ID Wahana valid
+    id = input("Masukkan ID Wahana: ")
     print("Riwayat:")
     i = 0
-    while ((array_penggunaan_tiket[i] != "*") and (i < 30)):  #Menelusuri Riwayat Penggunaan Tiket
+    done = False
+    while ((array_penggunaan_tiket[i] != "*") and (not done)):  #Menelusuri Riwayat Penggunaan Tiket
         if (array_penggunaan_tiket[i][2] == id):
             print(array_penggunaan_tiket[i][1], "|", array_penggunaan_tiket[i][0], "|",array_penggunaan_tiket[i][3])
-        i += 1
+        if i < 29 :
+            i += 1
+        else:
+            done = True
