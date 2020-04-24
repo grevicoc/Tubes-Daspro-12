dfb1d9dd601e63e74fa6ba24391decac24e5a80e
balam15 <64026206+balam15@users.noreply.github.com>
2020-04-24 16:51:40 +0700

    Update login_pemain.py

--- login_pemain.py
+++ login_pemain.py
@@ -3,9 +3,10 @@ def Login_User():
     username = input("Masukan username:")
     password = input("Masukan password:")
     for i in range(30):
-        if(array_user[i][3] == username):  
-            if(array_user[i][4] == password):
+        if (array_user[i][3] == username):  
+            if (array_user[i][4] == password):
                 print("Selamat bersenang-senang,",array_user[i][0],"!")
-        else:
-             print("Ups, password salah atau kamu tidak terdaftar dalam sistem kami. Silakan coba lagi!")
-             Login_User()
+                
+            else:
+                print("Ups, password salah atau kamu tidak terdaftar dalam sistem kami. Silakan coba lagi!")
+                Login_user()
