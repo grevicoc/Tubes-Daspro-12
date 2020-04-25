c5d60424a2a91854da60ecc334f7ff55083ade23
grevicoc <60306167+grevicoc@users.noreply.github.com>
2020-04-25 16:06:01 +0700

    Update [07]beli_tiket.py

--- [07]beli_tiket.py
+++ [07]beli_tiket.py
@@ -1,39 +1,3 @@
-import csv
-
-def validasi_umur(object_lahir, tanggal, batas_umur):     #object_umur adalah umur sang pengguna dan batas_umur adalah batasan umur wahana dan tanggal adalah hari ini
-    tahun_tanggal = int(tanggal[6:10])
-    bulan_tanggal = int(tanggal[3:5])
-    hari_tanggal = int(tanggal[0:2]) 
-    
-    tahun_user = int(object_lahir[6:10])
-    bulan_user = int(object_lahir[3:5])
-    hari_user = int(object_lahir[0:2])
-
-    if (hari_tanggal - hari_user) < 0:  #dengan penyederhanaan 1 bulan 30 hari dan tidak ada tahun kabisat
-        hari_tanggal += 30              #toh perbedaan 1-2 hari tidak terlalu signifikan
-        bulan_tanggal -= 1
-        if (bulan_tanggal - bulan_user) < 0:
-            bulan_tanggal += 12
-            tahun_tanggal -= 1
-            sum = (hari_tanggal-hari_user) + (30*(bulan_tanggal-bulan_user)) + (365*(tahun_tanggal-tahun_user))
-        else:
-            sum = (hari_tanggal-hari_user) + (30*(bulan_tanggal-bulan_user)) + (365*(tahun_tanggal-tahun_user))
-    else:
-        if (bulan_tanggal - bulan_user) < 0:
-            bulan_tanggal += 12
-            tahun_tanggal -= 1
-            sum = (hari_tanggal-hari_user) + (30*(bulan_tanggal-bulan_user)) + (365*(tahun_tanggal-tahun_user))
-        else:
-            sum = (hari_tanggal-hari_user) + (30*(bulan_tanggal-bulan_user)) + (365*(tahun_tanggal-tahun_user))
-
-    umur = sum/365
-    if batas_umur > umur:
-        return False
-    elif batas_umur <= umur:
-        return True
-
-
-
 def beli_tiket():
     id = input("Masukkan ID wahana: ")
     tanggal = input("Masukkan tanggal hari ini: ")
@@ -49,6 +13,13 @@ def beli_tiket():
                         while True:
                             if array_pembelian_tiket[j] == '*':
                                 array_pembelian_tiket[j] = [user[3], tanggal, id, str(tiket)] #memasukkan riwayat pembelian tiket
+                                break
+                            else:
+                                j+=1
+                        j = 0
+                        while True:
+                            if array_kepemilikan_tiket[j] == '*':
+                                array_kepemilikan_tiket[j] = [user[3], id, str(tiket)] #memasukkan riwayat kepemilikan tiket
                                 break
                             else:
                                 j+=1
