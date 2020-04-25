b4a4e9fe502992dad72cf63bde3559c912cb83f3
balam15 <64026206+balam15@users.noreply.github.com>
2020-04-25 16:38:32 +0700

    Update signup_pemain.py

--- signup_pemain.py
+++ signup_pemain.py
@@ -4,8 +4,12 @@ def Sign_Up():
     tinggi = input("Masukan tinggi badan pemain (cm):")
     username = input("Masukan username pemain:")
     password = input("Masukan password pemain:")
+    a = 0
     for i in range(30):
         if array_user[i] == "*" :
             array_user[i] = [nama,birth,tinggi,username,password,"guest",0,"no"]
-            
+            a = i
+        elif (i > a):
+            array_used[i] = "*"
+
     print("Selamat menjadi pemain,",nama,". Selamat bermain.")
