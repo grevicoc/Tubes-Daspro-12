f49a29120c11692af41d44929fb51dcc70d4df67
16519111 <64046206+16519111@users.noreply.github.com>
2020-04-26 09:28:41 +0700

    Update [06]cari_wahana.py

--- [06]cari_wahana.py
+++ [06]cari_wahana.py
@@ -6,6 +6,12 @@ def cari_wahana():
     print("Jenis batasan tinggi badan:")
     print("1. Lebih dari 130 cm")
     print("2. Tanpa batasan")
+    
+    k=0
+    while(array_user[k]!='*'):   # Menandai indeks array yang memiliki username yang sama dengan username saat login
+            if(array_user[k][3]==user):
+                d=k
+            k=k+1
 
     age = input("Batasan umur pemain: )
     while (age != '1') and (age != '2') and (age != '3'):
@@ -26,15 +32,25 @@ def cari_wahana():
             if int(array_wahana[i][3)] < 17:
                 if height == '1':
                     if int(array_wahana[i][4]) > 130:
-                        print(f"{array_wahana[i][0]} | {array_wahana[i][1]} | {array_wahana[i][2]}")
-                        mark = True
-                        i+=1
+                        if(array_user[d][7]=='yes'):
+                            print(f"{array_wahana[i][0]} | {array_wahana[i][1]} | {int(array_wahana[i][2])/2}")
+                            mark = True
+                            i+=1
+                        else:
+                            print(f"{array_wahana[i][0]} | {array_wahana[i][1]} | {array_wahana[i][2]}")
+                            mark = True
+                            i+=1
                     else:
                         i+=1
                 else:
-                    print(f"{array_wahana[i][0]} | {array_wahana[i][1]} | {array_wahana[i][2]}")
-                    mark = True
-                    i+=1
+                    if(array_user[d][7]=='yes'):
+                        print(f"{array_wahana[i][0]} | {array_wahana[i][1]} | {int(array_wahana[i][2])/2}")
+                        mark = True
+                        i+=1
+                    else:
+                        print(f"{array_wahana[i][0]} | {array_wahana[i][1]} | {array_wahana[i][2]}")
+                        mark = True
+                        i+=1
             else:
                 i+=1
     elif age == '2':
@@ -43,15 +59,25 @@ def cari_wahana():
             if int(array_wahana[i][3)] >= 17:
                 if height == '1':
                     if int(array_wahana[i][4]) > 130:
-                        print(f"{array_wahana[i][0]} | {array_wahana[i][1]} | {array_wahana[i][2]}")
-                        mark = True
-                        i+=1
+                        if(array_user[d][7]=='yes'):
+                            print(f"{array_wahana[i][0]} | {array_wahana[i][1]} | {int(array_wahana[i][2])/2}")
+                            mark = True
+                            i+=1
+                        else:
+                            print(f"{array_wahana[i][0]} | {array_wahana[i][1]} | {array_wahana[i][2]}")
+                            mark = True
+                            i+=1
                     else:
                         i+=1
                 else:
-                    print(f"{array_wahana[i][0]} | {array_wahana[i][1]} | {array_wahana[i][2]}")
-                    mark = True
-                    i+=1
+                    if(array_user[d][7]=='yes'):
+                        print(f"{array_wahana[i][0]} | {array_wahana[i][1]} | {int(array_wahana[i][2])/2}")
+                        mark = True
+                        i+=1
+                    else:
+                        print(f"{array_wahana[i][0]} | {array_wahana[i][1]} | {array_wahana[i][2]}")
+                        mark = True
+                        i+=1
             else:
                 i+=1
     else:
@@ -59,14 +85,24 @@ def cari_wahana():
         while array_wahana[i] != '*':
             if height == '1':
                     if int(array_wahana[i][4]) > 130:
-                        print(f"{array_wahana[i][0]} | {array_wahana[i][1]} | {array_wahana[i][2]}")
-                        mark = True
-                        i+=1
+                        if(array_user[d][7]=='yes'):
+                            print(f"{array_wahana[i][0]} | {array_wahana[i][1]} | {int(array_wahana[i][2])/2}")
+                            mark = True
+                            i+=1
+                        else:
+                            print(f"{array_wahana[i][0]} | {array_wahana[i][1]} | {array_wahana[i][2]}")
+                            mark = True
+                            i+=1
                     else:
                         i+=1
             else:
-                print(f"{array_wahana[i][0]} | {array_wahana[i][1]} | {array_wahana[i][2]}")
-                mark = True
-                i+=1
+                if(array_user[d][7]=='yes'):
+                        print(f"{array_wahana[i][0]} | {array_wahana[i][1]} | {int(array_wahana[i][2])/2}")
+                        mark = True
+                        i+=1
+                    else:
+                        print(f"{array_wahana[i][0]} | {array_wahana[i][1]} | {array_wahana[i][2]}")
+                        mark = True
+                        i+=1
     if mark == False:
         print("Tidak ada wahana yang sesuai dengan pencarian kamu.")
